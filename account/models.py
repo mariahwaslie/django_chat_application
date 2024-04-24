2086f6c6cfee4db9e167499e1bf2f03f69c892c3
mariahwaslie <55723431+mariahwaslie@users.noreply.github.com>
2024-04-24 13:30:29 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -11,11 +11,3 @@ class User(User,auth.models.PermissionsMixin):
     def __str__(self):
         #username is a built-in attribute from User
         return f'@{self.username}'
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     username = models.CharField(max_length=50, unique=True)
-#     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-#     def __str__(self):
-#         return f'@{self.username}'
